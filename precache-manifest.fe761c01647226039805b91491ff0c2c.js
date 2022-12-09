43f820a6fe66e5cbb49ac1b6b78825a604e0acee
sparteek65 <sparteek65@gmail.com>
2022-12-09 19:04:43 +0530

    Updates

---
+++ precache-manifest.fe761c01647226039805b91491ff0c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e55989cad28499f1210",
+    "url": "/what_to_watch/static/css/main.f3512434.chunk.css"
+  },
+  {
+    "revision": "2e55989cad28499f1210",
+    "url": "/what_to_watch/static/js/main.2e55989c.chunk.js"
+  },
+  {
+    "revision": "de53d44062204e8b07c9",
+    "url": "/what_to_watch/static/js/1.de53d440.chunk.js"
+  },
+  {
+    "revision": "025d94bc4efa475ddd8b",
+    "url": "/what_to_watch/static/js/2.025d94bc.chunk.js"
+  },
+  {
+    "revision": "8740bb14eeb10cc629eb",
+    "url": "/what_to_watch/static/js/runtime~main.8740bb14.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/what_to_watch/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6df3bd5680b05ec5b17ef2bf1d23af60",
+    "url": "/what_to_watch/index.html"
+  }
+];
